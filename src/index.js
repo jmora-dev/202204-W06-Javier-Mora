c384bfcd5bf1b62fd6257a0da819086a234581f4
jmora-dev <dam2mora@gmail.com>
2022-05-15 13:02:50 +0200

    Initial run logic

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import { Game } from './gameOfLife/classes/Game.js';
 
 const game = new Game(15, 20, 'app', 'onClickCell');
 
-// eslint-disable-next-line no-unused-vars
 window.onClickCell = (x, y) => {
   game.onClickCell(+x, +y);
+};
+
+window.onStart = () => {
+  game.start();
+};
+
+window.onStop = () => {
+  game.stop();
 };
